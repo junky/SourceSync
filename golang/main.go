8d081d98a5beef336c1bd54bb8ba1326dd9ba662
Alex Koval <alexander@archer-soft.com>
2015-06-23 18:25:00 +0300

    FS events + filepath.Walk

---
+++ golang/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"os"
+
+	"github.com/go-fsnotify/fsnotify"
+)
+
+func ExampleNewWatcher() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("D:\\Junky\\Projects\\SmartLing\\SourceSync\\golang")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
+
+func ScanAllFiles(location string) (err error) {
+
+    var scan = func(path string, fileInfo os.FileInfo, inpErr error) (err error) {
+	if fileInfo.IsDir() {
+		fmt.Println(path)
+	}
+	return
+    }
+
+
+    err = filepath.Walk(location, scan)
+
+    return
+}
+
+func main() {
+	fmt.Println("Hello, Alex")
+	ScanAllFiles("D:\\Junky\\Projects\\SmartLing\\SourceSync\\golang\\")
+	ExampleNewWatcher()
+}
