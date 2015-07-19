b062ee5f934edd9ab91232073a3328546b1797e2
Alex Koval <alexander@archer-soft.com>
2015-07-19 23:46:20 +0300

    System tests of sync_client

--- golang/sync_client_test.go
+++ golang/sync_client_test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
 	"testing"
+	"time"
 )
 
 func ClearTestsFolder() {
@@ -15,7 +20,39 @@ func CreateTestsFolder() {
 	ioutil.WriteFile("tests/Folder-1/test1.txt", []byte("text"), 0666)
 }
 
+func BuildClient() {
+	cmd := exec.Command("go", "build", "sync_client.go")
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func TestFSEvents(t *testing.T) {
 	ClearTestsFolder()
 	CreateTestsFolder()
+	BuildClient()
+
+	cmd := exec.Command("./sync_client")
+	stdout, _ := cmd.StdoutPipe()
+
+	buff := bufio.NewScanner(stdout)
+	go func() {
+		for buff.Scan() {
+			fmt.Printf("Stdout: %s\n", buff.Text())
+		}
+	}()
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(1000 * time.Millisecond)
+
+	fmt.Println("Kill")
+	if err := cmd.Process.Kill(); err != nil {
+		log.Fatal("failed to kill: ", err)
+	}
+
+	cmd.Wait()
+
 }
